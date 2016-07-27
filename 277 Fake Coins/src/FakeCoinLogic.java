bb7683b28075c0096cf8b06a5d74c783c1601e69
Drethin <a.hong@live.ca>
2016-07-27 13:06:41 -0500

    Too hard!!

--- 277 Fake Coins/src/FakeCoinLogic.java
+++ 277 Fake Coins/src/FakeCoinLogic.java
@@ -1,4 +1,4 @@
-import java.util.Hashtable;
+import java.util.ArrayList;
 
 /**
  * Created by Alex on 27/07/2016.
@@ -10,36 +10,74 @@ class FakeCoinLogic {
     String lean;
     String coinList;
     String equalCoins;
-    Hashtable<String, Integer> coinWeights;
+    ArrayList<Integer> validCombos;
+    int[][]leftCombos;
+    int[][]rightCombos;
+
     FakeCoinLogic(String[] input){
+        validCombos = new ArrayList<Integer>();
         coinList = "";
         for (String s1: input) {
             line = s1.split(" ");
             left = line[0];
             right = line[1];
             lean = line[2];
-
-            if(lean.equals("left")){
-                updateCoins(left, 2);
-                updateCoins(right, 1);
-            }else if(lean.equals("right")){
-                updateCoins(left, 1);
-                updateCoins(right, 2);
-            }else{
-                updateCoins(left+right, 0);
+            int numCoins = left.length() + right.length();
+            int[][] combos = new int[numCoins*numCoins][numCoins];
+            for(int i = 0; i < numCoins*numCoins; i++){
+                for(int j = 0; j < numCoins; j++){
+                    combos[i][j] = (i & (1 << j));
+                }
             }
-
         }
     }
-    void updateCoins(String coins, int weight){
-        for(String s: coins.split("")){
-            if(!coinList.contains(s)) {
-                coinList += s;
-                coinWeights.put(s, weight);
-            }else{
-                coinWeights.replace(s, coinWeights.get(s)+weight);
+    public static void main(String[]args){
+        int [][]combos = new int[4][2];
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < 2; j++){
+                combos[i][j] = (i >> j)%2;
+                System.out.print(combos[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    private int[][] combos(String line){
+        int [][]combos = new int[line.length()*line.length()][line.length()];
+        for(int i = 0; i < line.length()*line.length(); i++){
+            for(int j = 0; j < line.length(); j++){
+                combos[i][j] = (i >> j)%2;
             }
         }
+        return combos;
+    }
+    private void addValidCombos(){
+        rightCombos = combos(right);
+        leftCombos = combos(left);
+        for(int[]i:leftCombos){
+            for(int[]j:rightCombos){
+                if(lean.equals("equal")) {
+                    if (sumArray(i) == sumArray(j)) {
+
+                    }
+                }else if(lean.equals("left")){
+                    if (sumArray(i) > sumArray(j)) {
+
+                    }
+                }else if(lean.equals("right")){
+                    if (sumArray(i) < sumArray(j)) {
+
+                    }
+                }
+            }
+        }
+    }
+
+    private int sumArray(int[]array){
+        int sum = 0;
+        for(int i: array){
+            sum += i;
+        }
+        return sum;
     }
     private class Coin{
         int weight;
