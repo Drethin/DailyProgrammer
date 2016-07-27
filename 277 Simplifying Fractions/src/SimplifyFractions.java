85c22998a0543c448eb1d2b346d637373016af55
Drethin <a.hong@live.ca>
2016-07-27 10:41:30 -0500

    Does this work?

--- 277 Simplifying Fractions/src/SimplifyFractions.java
+++ 277 Simplifying Fractions/src/SimplifyFractions.java
@@ -1,5 +1,32 @@
 /**
  * Created by Alex on 27/07/2016.
  */
-public class SimplifyFractions {
+class SimplifyFractions {
+    SimplifyFractions(){
+
+        System.out.println(simplify("5 10"));
+
+    }
+    public static void main(String []args){
+        new SimplifyFractions();
+    }
+    String simplify(String fraction){
+        int left;
+        int right;
+        int gcd;
+        String[] tmp = fraction.split(" ");
+        left = Integer.parseInt(tmp[0]);
+        right = Integer.parseInt(tmp[1]);
+        gcd = gcd(right, left);
+        return (left/gcd + " " + right/gcd);
+    }
+    int gcd(int a, int b){
+        int c = a;
+        while (c!=0){
+            c=a%b;
+            a=b;
+            b=c;
+        }
+        return a;
+    }
 }
